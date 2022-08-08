1cec341b1e7844b73afe035526a3f36d51124f63
rav4353 <ravanthsri20@gmail.com>
2022-08-08 12:05:26 +0530

    hi

--- js/script.js
+++ js/script.js
@@ -34,8 +34,47 @@ console.log(Boolean(z));
 
 //array
 
+//declare arrays with the const keyword.
+
+
+const car = ["Saab", "Volvo", "BMW"];
+console.log(car);
+
+
+//create an array, and then provide the elements:
+const cars = [];
+cars[0]= "Saab";
+cars[1]= "Volvo";
+cars[2]= "BMW";
+console.log(cars);
 
 
 //object
 
+
+// Create an object:
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
+// Display some data from the object:
+
+
+console.log(person.firstName + " is " + person.age + " years old.");
 // Regular Expression
+
+//i
+
+let text = "Visit W3Schools";
+let result = text.match(/w3schools/i);
+console.log(result);
+
+//g
+
+let texts = "Is this all there is?";
+let results = texts.match(/is/g);
+console.log(results); 
+
+//m
+
+let textss = "\nIs th\nis it?";
+let resultss = textss.match(/^is/m);
+console.log("Resultss is: " + (resultss));
