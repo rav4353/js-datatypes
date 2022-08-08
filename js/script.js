bbb2d7964fdc5de4ff0bbc9b5604f703d762f842
rav4353 <ravanthsri20@gmail.com>
2022-08-08 10:00:10 +0530

    hi

---
+++ js/script.js
@@ -0,0 +1,41 @@
+//primitive data types
+
+
+//adding a number and a string, JavaScript will treat the number as a string.
+
+// ex for string & number
+
+let x = 16 + "Volvo";
+console.log(x);
+
+
+//boolean
+
+
+console.log(Boolean(10 > 9));
+
+
+//null
+
+
+let y = null;
+console.log(Boolean(y));
+
+
+//undefined
+
+
+let z;
+console.log(Boolean(z));
+
+
+//Non primitive data types
+
+
+//array
+
+
+
+//object
+
+// Regular Expression
